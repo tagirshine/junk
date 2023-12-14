88e5faffc1bce5e95a84f405b7ac034e2e6b9c38
Tagir Satlykov <tagirjob@gmail.com>
2023-12-14 22:50:54 +0100

    vasya

---
+++ backend/oldstaff.js
@@ -0,0 +1,33 @@
+"use strict";
+/*
+import localization from "./localization";
+
+
+bot.on('message', async (msg: any ) : Promise<void>  => { ///*, match
+  const chatId = msg.chat.id;
+
+  if (msg.text === localization.add) {
+    console.log('measo')
+  }
+
+  if (msg?.location) {
+    console.log(msg.location)
+    await createLocation(msg.location.latitude, msg.location.longitude)
+    console.log('локация добавленна')
+    bot.sendMessage(chatId, `локация добавленна ${msg.location.latitude} ${msg.location.longitude}`);
+  }
+
+  const options : SendMessageOptions = { //ReplyKeyboardMarkup
+    reply_markup: {
+      resize_keyboard: true,
+      one_time_keyboard: true,
+      keyboard: [
+        [elem],
+      ],
+    },
+  };
+  bot.sendMessage(chatId, "Спасибо за участие!", options);
+
+});
+
+*/
